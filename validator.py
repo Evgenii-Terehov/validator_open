ceb6b2ff87198fb0182de5b115665ab611c4ec3a
Evgenii-Terekhov <44933011+Evgenii-Terekhov@users.noreply.github.com>
2020-12-13 23:43:59 +0300

    Update validator.py

--- validator.py
+++ validator.py
@@ -2,23 +2,23 @@ import os
 import json
 from jsonschema import Draft3Validator
 
-for schema in os.listdir('./schema'):
-    with open(schema, 'r') as s:
-        data_s = json.load(s)
+with open('example_file.html', 'a') as out:
+    for schema in os.listdir('./schema'):
+        with open(schema, 'r') as s:
+            data_s = json.load(s)
 
-    for file in os.listdir('./event'):
-        with open(file, 'r') as f:
-            data_f = json.load(f)
-            v = Draft3Validator(data_s)
-            for error in sorted(v.iter_errors(data_f), key=str):
-                TODO = ''
-                if 'is a required property' in error.message:
-                    TODO = 'Это обязательно к заполнению поле'
-                elif 'None is not of type' in error.message:
-                    TODO = 'Невозможно оставить поле пустым'
-                elif 'is not of type' in error.message:
-                    TODO = 'Введите данные в соотвествии с требуемым типом'
-                with open('example_file.html', 'a') as out:
+        for file in os.listdir('./event'):
+            with open(file, 'r') as f:
+                data_f = json.load(f)
+                v = Draft3Validator(data_s)
+                for error in sorted(v.iter_errors(data_f), key=str):
+                    TODO = ''
+                    if 'is a required property' in error.message:
+                        TODO = 'Это обязательно к заполнению поле'
+                    elif 'None is not of type' in error.message:
+                        TODO = 'Невозможно оставить поле пустым'
+                    elif 'is not of type' in error.message:
+                        TODO = 'ID пользователя должен состоять только из цифр или быть пустым'
                     out.write(
                         f'<table>'
                         f'  <tbody>'
